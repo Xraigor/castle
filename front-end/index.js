6662633b180eaf7b65176ad4a041e87c7809355a
Xraigor <wuzhiheng1996@gmail.com>
2019-02-19 06:24:29 +0800

    Add the href of the restaurant on the searching result. So you can go to the michelin website of the restaurant to get more details.

--- front-end/index.js
+++ front-end/index.js
@@ -6,11 +6,13 @@
     const template = actors.map(actor => {
       return `
       <h1>
+        <a class = "inner">
         <div class="row">
           <span>Restaurant is ${actor.name}.</span></br>
           <span>Price is from ${actor.minPrice}&#8364 to ${actor.maxPrice}&#8364.</span></br>
           <span>Rating  ${actor.rating}.</span>
         </div>
+        </a>
       </h1>
 
       `;
@@ -20,6 +22,10 @@
     fragment.appendChild(div);
     document.querySelector('#actors').innerHTML = '';
     document.querySelector('#actors').appendChild(fragment);
+    let changeHrefs = document.getElementsByClassName("inner");
+    for(var i=0;i<changeHrefs.length;i++){
+    				changeHrefs[i].setAttribute("href",actors[i].href);
+    			}
   };
 
   const buttonName = document.querySelector('#searchName');
